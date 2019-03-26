05583341a3425cb4b4178f06c5708355dcd16bea
J Robinson <jrobinson@touro.edu>
2019-03-25 20:05:26 -0400

    Add project files.

---
+++ FunWithOrm/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithOrm
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class DataLayer : DbContext
+    {
+        public DbSet<Student> Students { get; set; }  // db Table
+        public DbSet<Person> Persons { get; set; }  // db Table
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer<DataLayer>(new DropCreateDatabaseIfModelChanges<DataLayer>());
+
+            DataLayer dal = new DataLayer();
+            
+            Console.WriteLine(dal.Students.Count());
+           
+            var jb = new Student{FirstName = "Dovie", LastName = "bob"};
+            dal.Students.Add(jb);
+
+            dal.SaveChanges();
+
+            foreach (var student in dal.Students.ToList().Where(s => s.LastName == Reverse(s.LastName)))
+            {
+                Console.WriteLine($"{student.StudentId,10}{student.FirstName,10}{student.LastName,10}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string Reverse(string s)
+        {
+            return s.Length <= 1 ? s : Reverse(s.Substring(1, s.Length - 1)) + s[0];
+        }
+    }
+}
